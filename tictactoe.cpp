74c765c2710aa2afa113ce93b06afd71be239516
ridham sharma <ridham.sharmarce@gmail.com>
2025-11-12 18:38:47 +0530

    add:game-initializer

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,7 +1,34 @@
 #include<iostream>
 using namespace std;
 
+char grid[3][3];
+
+void gameStart(){
+    char value = '1';
+    for(int i = 0 ; i <3;i++){
+        for(int j = 0 ; j < 3 ; j++){
+            grid[i][j]=value++;
+        }
+
+    }
+}
+
+
+void gameprint(){
+    
+    for(int i = 0 ; i <3;i++){
+        for(int j = 0 ; j < 3 ; j++){
+            cout<<grid[i][j]<<" ";
+        }
+        cout<<endl;
+
+    }
+}
+
 int main(){
+    gameStart();
+    gameprint();
+
 
 return 0;
 }
